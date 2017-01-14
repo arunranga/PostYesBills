5b5ef46f76aca5a7b25b875c7be6f57d8812e86d
Cole Murphy <cole.murphy@venuebook.com>
2017-01-14 15:53:43 -0500

    Update routing logic for home and individual bills, rende bills dynamically, begin building functions for when a bill is clicked.

--- js/App.jsx
+++ js/App.jsx
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 
-import BillList from './components/BillList';
-import HotBill from './components/HotBill';
-
 export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-		<div className="container home">
-			<HotBill />
-			<BillList />
-		</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				{this.props.children}
+			</div>
+		);
+	}
 }
--- js/index.jsx
+++ js/index.jsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import App from './App';
+import Home from './containers/Home';
+import Bill from './Bill';
 
 const router = (
   <Router history={hashHistory}>
     <Route path="/" component={App}>
+    	<IndexRoute component={Home} />
+    </Route>
+    <Route path="/bill/:id" component={Bill}>
     </Route>
   </Router>
 );
