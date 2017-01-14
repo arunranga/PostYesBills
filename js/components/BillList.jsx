7ae320290157ade496f5ccbcb8607d7ac827283b
Cole Murphy <cole.murphy@venuebook.com>
2017-01-14 17:14:02 -0500

    Begin adding comments and noations to bills in BillList.

--- js/components/BillList.jsx
+++ js/components/BillList.jsx
@@ -16,6 +16,8 @@ export default class BillList extends React.Component {
 				<li className="bill-list__bill" data-id={`76${x}`} key={x} onClick={this.handleBillClick.bind(this)}>
 					<h2 className="bill-list__bill-number">H.R. 76{x}</h2>
 					<p className="bill-list__bill-text">This is the text of bill number 76{x}. This is the text of bill number 76{x}. This is the text of bill number 76{x}. This is the text of bill number 76{x}. This is the text of bill number 76{x}. This is the text of bill number 76{x}. This is the text of bill number 76{x}. This is the text of bill number 76{x}. This is the text of bill number 76{x}. This is the text of bill number 76{x}. This is the text of bill number 76{x}.</p>
+					<p className="bill-list__bill-comments">Comments: {x}</p>
+					<p className="bill-list__bill-notations">Notations: {x}</p>
 				</li>
 			)
 		}
