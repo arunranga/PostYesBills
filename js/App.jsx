4cdc0303411662d63e2b6ed633d16c2655aa1c8f
Cole Murphy <cole.murphy@venuebook.com>
2017-01-14 15:18:13 -0500

    Begin building static dumb components.

--- js/App.jsx
+++ js/App.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import BillList from './components/BillList';
+import HotBill from './components/HotBill';
+
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello world!
+		<div className="container home">
+			<HotBill />
+			<BillList />
+		</div>
       </div>
     );
   }
---
+++ js/components/HotBill.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class HotBill extends React.Component {
+	render () {
+		return (
+			<div className="hot-bill">
+				<h2 className="bill-name">Hot bill!!</h2>
+				<p className="bill-number">H.R. 763</p>
+				<p className="bill-text">This is the text of the bill. This is the text of the bill. This is the text of the bill. This is the text of the bill. This is the text of the bill. This is the text of the bill. This is the text of the bill. This is the text of the bill. This is the text of the bill. This is the text of the bill. This is the text of the bill.</p>
+			</div>
+		);
+	}
+}
