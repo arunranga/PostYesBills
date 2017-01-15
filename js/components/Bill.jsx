170f2eb851cd90f6b44095802cda2e7e5b3f8412
Esther Pong <e.pong12@gmail.com>
2017-01-15 08:22:09 -0500

    add links to bill pages

--- js/components/Bill.jsx
+++ js/components/Bill.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export default class Bill extends Component {
 
@@ -11,16 +12,18 @@ export default class Bill extends Component {
     let { comments, id, title, text, notations, number } = this.props.bill;
 
     return (
-      <li className="bill-list__bill" data-id={number} key={number} onClick={this.handleBillClick.bind(this)}>
-        <div className="bill-list__bill-id">
-          <h3 className="bill-list__bill-number">{number}</h3>
-        </div>
-        <div className="bill-list__bill-content">
-          <h2 className="bill-list__bill-title">{title}</h2>
-          <p className="bill-list__bill-text">{text}</p>
-          <p className="bill-list__bill-data">Comments: {comments}, Notations: {notations}</p>
-        </div>
-      </li>
+      <Link to={`/bills/${id}`} >
+        <li className="bill-list__bill" data-id={number} key={number} onClick={this.handleBillClick.bind(this)}>
+          <div className="bill-list__bill-id">
+            <h3 className="bill-list__bill-number">{number}</h3>
+          </div>
+          <div className="bill-list__bill-content">
+            <h2 className="bill-list__bill-title">{title}</h2>
+            <p className="bill-list__bill-text">{text}</p>
+            <p className="bill-list__bill-data">Comments: {comments}, Notations: {notations}</p>
+          </div>
+        </li>
+      </Link>
     );
   }
 }
