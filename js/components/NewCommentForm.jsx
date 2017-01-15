0b8810bcf35cd4f2a0c7d00d28811409d326a566
Esther Pong <e.pong12@gmail.com>
2017-01-15 09:22:25 -0500

    add comment at selection location

--- js/components/NewCommentForm.jsx
+++ js/components/NewCommentForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 export default class NewCommentForm extends React.Component {
   render () {
     return (
@@ -6,7 +7,24 @@ export default class NewCommentForm extends React.Component {
         className="comments-list-container"
       >
         <div>Comment form!</div>
+
+        <form style={ {marginTop: `${this.props.location}px`} }
+          className={ this.props.display ? "comment-form" : "hide" }
+          onSubmit={this.props.addComment}>
+
+          <label>Comment: <br/>
+            <textarea
+              value={this.props.body}
+              onChange={this.props.update}></textarea>
+          </label>
+
+          <button onClick={this.props.addComment}
+            value={this.props.parentCommentId}>
+            Submit
+          </button>
+
+        </form>
       </div>
     );
   }
-};
+}
