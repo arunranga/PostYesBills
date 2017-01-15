3d96834db37899afca5764fc7851c85cfcbb8cda
Petr Gazarov <petr@policygenius.com>
2017-01-15 09:10:33 -0500

    Add dummy data

--- js/containers/Home.jsx
+++ js/containers/Home.jsx
@@ -4,12 +4,12 @@ import BillListContainer from '../containers/BillListContainer';
 import HotBillContainer from '../containers/HotBillContainer';
 
 export default class Home extends Component {
-	render() {
-		return (
-			<div className="container home">
-				<HotBillContainer />
-				<BillListContainer />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container home">
+        <HotBillContainer />
+        <BillListContainer />
+      </div>
+    );
+  }
 }
